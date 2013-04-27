f5a4062b15f7cbc9068801b47940a20e2e9558bb
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-04-27 20:51:41 +0100

    find the starting node

--- js/geoPlanner.js
+++ js/geoPlanner.js
@@ -1,7 +1,28 @@
 
 var GeoPlanner = (function(self) {
-  self.findShortestPath = function(listOfPoints) {
-    return [];
+
+  var pointDistance = function(p1, p2) {
+    return distance(p1.lat, p1.long, p2.lat, p2.long);
+  };
+
+  var moveFirstToStart = function(listOfPoints, startingPoint) {
+    var startIdx = -1;
+    var minDist = 1e10;
+    for (var i in listOfPoints) {
+      var dist = pointDistance(startingPoint, listOfPoints[i]);
+      if (dist < minDist) {
+        minDist = dist;
+        startIdx = i;
+      }
+    }
+    if (startIdx >= 0) {
+      return [listOfPoints[startIdx]].concat(listOfPoints.splice(startIdx, 1));
+    }
+    return listOfPoints;
+  };
+
+  self.findShortestPath = function(listOfPoints, startingPoint) {
+    return moveFirstToStart(listOfPoints, startingPoint);
   };
 
   return self;
--- tests/spec/geoPlannerSpec.js
+++ tests/spec/geoPlannerSpec.js
@@ -4,4 +4,26 @@ describe("GeoPlanner", function() {
     expect(ret instanceof Array).toBeTruthy();
   });
 
+  it("should start closest to the starting point", function() {
+    var ret = GeoPlanner.findShortestPath(
+      [
+          {lat:51.516354 , long: -0.130548}, // TCR
+          {lat:51.513149 , long: -0.12454},  // covent garden
+      ],
+      {lat:51.511214, long:-0.119824} // strand / waterloo bridge
+    );
+    expect(ret[0]).toEqual({lat:51.513149 , long: -0.12454});
+  });
+
+  it("should return the same amount of points", function() {
+    var ret = GeoPlanner.findShortestPath(
+      [
+          {lat:51.516354 , long: -0.130548}, // TCR
+          {lat:51.513149 , long: -0.12454},  // covent garden
+      ],
+      {lat:51.511214, long:-0.119824} // strand / waterloo bridge
+    );
+    expect(ret.length).toEqual(2);
+
+  });
 });
