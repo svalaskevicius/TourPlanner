ac04d62dfafa84f4380995b445c9185cb1614d54
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-04-27 21:46:13 +0100

    add traveling salesman approx [nearest neighbour]

--- js/geoPlanner.js
+++ js/geoPlanner.js
@@ -5,24 +5,63 @@ var GeoPlanner = (function(self) {
     return distance(p1.lat, p1.long, p2.lat, p2.long);
   };
 
-  var moveFirstToStart = function(listOfPoints, startingPoint) {
+  var findClosestPoint = function(listOfPoints, target) {
     var startIdx = -1;
     var minDist = 1e10;
     for (var i in listOfPoints) {
-      var dist = pointDistance(startingPoint, listOfPoints[i]);
+      var dist = pointDistance(target, listOfPoints[i]);
       if (dist < minDist) {
         minDist = dist;
         startIdx = i;
       }
     }
+    return startIdx;
+  };
+
+  var swapToTheStart = function(list, idx) {
+    var temp = list[0];
+    list[0] = list[idx];
+    list[idx] = temp;
+    return list;
+  };
+
+  var moveFirstToStart = function(listOfPoints, startingPoint) {
+    var startIdx = findClosestPoint(listOfPoints, startingPoint);
     if (startIdx >= 0) {
-      return [listOfPoints[startIdx]].concat(listOfPoints.splice(startIdx, 1));
+      return swapToTheStart(listOfPoints, startIdx);
     }
     return listOfPoints;
   };
 
+  /**
+   * leaves the first item as the starting point,
+   * and only sorts the rest of the list
+   */
+  var travelingSalesman = function(listOfPoints) {
+    if (1 >= listOfPoints.length) {
+      return listOfPoints;
+    }
+    var theRest = listOfPoints;
+    var first = theRest.splice(0, 1)[0];
+    var closestIdx = findClosestPoint(theRest, first);
+    if (closestIdx >= 0) {
+      var nextNode = listOfPoints[closestIdx];
+      return [first].concat(
+        travelingSalesman(
+          swapToTheStart(
+            listOfPoints, closestIdx
+          )
+        )
+      );
+    } else {
+      return listOfPoints;
+    }
+  };
+
   self.findShortestPath = function(listOfPoints, startingPoint) {
-    return moveFirstToStart(listOfPoints, startingPoint);
+    return travelingSalesman(
+      moveFirstToStart(listOfPoints, startingPoint)
+    );
   };
 
   return self;
--- tests/spec/geoPlannerSpec.js
+++ tests/spec/geoPlannerSpec.js
@@ -1,8 +1,10 @@
 describe("GeoPlanner", function() {
+
   it("should return an array", function() {
     var ret = GeoPlanner.findShortestPath([]);
     expect(ret instanceof Array).toBeTruthy();
   });
+
 
   it("should start closest to the starting point", function() {
     var ret = GeoPlanner.findShortestPath(
@@ -15,6 +17,7 @@ describe("GeoPlanner", function() {
     expect(ret[0]).toEqual({lat:51.513149 , long: -0.12454});
   });
 
+
   it("should return the same amount of points", function() {
     var ret = GeoPlanner.findShortestPath(
       [
@@ -24,6 +27,31 @@ describe("GeoPlanner", function() {
       {lat:51.511214, long:-0.119824} // strand / waterloo bridge
     );
     expect(ret.length).toEqual(2);
+  });
 
+
+  it("should calculate the shortest path given a straight line", function() {
+    var ret = GeoPlanner.findShortestPath(
+      [
+          {lat:51.516354 , long: -0.130548}, // TCR
+          {lat:51.513149 , long: -0.12454},  // covent garden
+          {lat:51.524712 , long: -0.138316}, // warren street
+          {lat:51.520493 , long: -0.13393},  // goodge street
+      ],
+      {lat:51.511214, long:-0.119824} // strand / waterloo bridge
+    );
+    expect(ret).toEqual(
+      [
+          {lat:51.513149 , long: -0.12454},  // covent garden
+          {lat:51.516354 , long: -0.130548}, // TCR
+          {lat:51.520493 , long: -0.13393},  // goodge street
+          {lat:51.524712 , long: -0.138316}, // warren street
+      ]
+    );
+  });
+
+  it("should calculate the shortest path given a circle", function(){
+    expect("pending").toEqual("");
   });
 });
+
