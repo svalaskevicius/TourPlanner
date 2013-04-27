616553418df115cb6e8eae831ae08fc1d89384ab
Arturas Smorgun <arturas@smorgun.lt>
2013-04-27 23:11:03 +0100

    twitter bootstrap and slider

--- js/frontend.js
+++ js/frontend.js
@@ -1,46 +1,24 @@
-var map;
-//        function grabMyPosition() {
-//            if (navigator.geolocation) {
-//                navigator.geolocation.getCurrentPosition(centerMe);
-//            } else {
-//                alert("You don't support this");
-//            }
-//        }
 function centerMe(center) {
     var coords = new google.maps.LatLng(
         center.coords.latitude,
         center.coords.longitude
     );
-
-    //map.setCenter(coords);
-    // or
     map.panTo(coords);
 }
-YUI({
-    //Last Gallery Build of this module
-    gallery: 'gallery-2012.09.05-20-01'
-}).use('gallery-google-maps-loader', 'node', 'event', function (Y) {
-        new Y.GoogleMapsLoader().load({
-            language: 'en',
-            libraries: [
-                'adsense',
-                'geometry'
-            ],
-            region: 'ES',
-            sensor: true,
-            version: '3.4'
-        }).on('success', function () {
-                map = new google.maps.Map(Y.Node.getDOMNode(Y.one('#map')), {
-                    zoom: 10,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                });
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(centerMe);
-                } else {
-                    alert("You don't support this");
-                }
-                mapDiv = Y.Node.getDOMNode(Y.one('#map'));
-//                mapDiv.style.width = '100%';
-//                google.maps.event.trigger(map, 'resize');
-            });
+var map;
+$(document).ready(function () {
+    var mapOptions = {
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(centerMe);
+    } else {
+        alert("You don't support this");
+    }
+
+    $('.slider').slider().on('slide', function(slide) {
+        $('#radiusvalue').html(slide.value);
     });
+});
