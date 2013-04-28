3b5ba86591fab28de373cc3f5ef6cef9e8ef36d6
Arturas Smorgun <arturas@smorgun.lt>
2013-04-28 07:32:05 +0100

    pull location images from flickr

--- js/frontend-locations.js
+++ js/frontend-locations.js
@@ -36,7 +36,7 @@ function receivedLocations(data) {
             position: new google.maps.LatLng(val.geometry.location.lat, val.geometry.location.lng),
             map: map,
             title: val.name,
-            icon: new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|009900"),
+            icon: new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|009900")
         });
         var newLocationInfo = val;
         currentLocationsInfoArray[newKey] = newLocationInfo;
@@ -45,7 +45,8 @@ function receivedLocations(data) {
             currentSelectedItem = newLocation;
             currentSelectedItemInfo = newLocationInfo;
             infoWindow.setContent(buildContent(newLocation, newLocationInfo));
-            infoWindow.open(map, currentLocationsArray[newKey])
+            infoWindow.open(map, currentLocationsArray[newKey]);
+            addMoreInfo(newLocation, newLocationInfo);
         });
     });
     $.each(data.value.items, function (key, val) {
@@ -63,7 +64,8 @@ function receivedLocations(data) {
                 currentSelectedItem = newLocation;
                 currentSelectedItemInfo = newLocationInfo;
                 infoWindow.setContent(buildContent(newLocation, newLocationInfo));
-                infoWindow.open(map, newLocation)
+                infoWindow.open(map, newLocation);
+                addMoreInfo(newLocation, newLocationInfo);
             });
         }
     });
@@ -71,13 +73,55 @@ function receivedLocations(data) {
 
 function buildContent(item, itemInfo)
 {
-    res = '<strong>' + itemInfo.name + '</strong>';
+    res = '<div><strong>' + itemInfo.name + '</strong>&nbsp;&nbsp;';
     if (isInSelected(item.getTitle())) {
-        res += '<div align="right"><a href="javascript:unselectCurrent()" class="btn btn-danger btn-small">Remove</a></div>';
+        res += '<a href="javascript:unselectCurrent()" class="btn btn-danger btn-small">Remove</a></div>';
     } else {
-        res += '<div align="right"><a href="javascript:selectCurrent()" class="btn btn-success btn-small">Add</a></div>';
+        res += '<a href="javascript:selectCurrent()" class="btn btn-success btn-small">Add</a></div>';
     }
+    res += '<div style="height: 200px" id="infoWindowDetails' + itemInfo.reference + '"><div class="flickrimages"></div></div>';
     return res;
+}
+
+function addMoreInfo(item, itemInfo)
+{
+    $.ajax({
+        dataType: 'json',
+//        url: 'file:///Users/asarturas/Sites/TourPlanner/fixtures/sample.json',
+        url: 'http://query.yahooapis.com/v1/public/yql',
+        data: {
+            q: 'select * from flickr.photos.search where api_key="5ce0a696481d9149ea9269ea3ba00d98" and has_geo="true" and lat=' + item.getPosition().lat() + ' and lon=' + item.getPosition().lng() + ' and accuracy=11 and radius=0.1 and min_upload_date="2005-01-01"',
+            format: 'json',
+            diagnostics: 'true',
+            env: 'store://datatables.org/alltableswithkeys'
+//            _id: '8186a63d136a6ce1302a2d5297980ffd',
+//            _render: 'json',
+//            radius: $('#radiusvalue').html(),
+//            location: currentPosition.getPosition().lat() + ',' + currentPosition.getPosition().lng(),
+//            googleApiKey: 'AIzaSyC2L22kbDS3QqQlFb5r0Yc5TgnnhratbCA'
+        },
+        success: function(data) {
+            var div = $('#infoWindowDetails' + itemInfo.reference);
+            div.find('.flickrimages').html(
+                data.query.results.photo.map(
+                    function(elem) {
+                        return '<img src="http://farm' + elem.farm + '.staticflickr.com/' + elem.server + '/' + elem.id + '_' + elem.secret + '_t.jpg">';
+                    }
+                ).join('')
+            );
+            console.log(data.query.results.photo.map(
+                function(elem) {
+                    return '<img src="http://farm' + elem.farm + '.staticflickr.com/' + elem.server + '/' + elem.id + '_' + elem.secret + '_t.jpg"';
+                }
+            ).join());
+        }
+    });
+
+    console.log($('#infoWindowDetails' + itemInfo.reference));
+
+//    $('#infoWindowDetails' + itemInfo.reference).html(
+//        '<img src="https://maps.googleapis.com/maps/api/place/photo?key=8186a63d136a6ce1302a2d5297980ffd&photoreference=' + itemInfo.photos.photo_reference + '&sensor=true&maxwidth=256"/>'
+//    );
 }
 
 function selectCurrent()
